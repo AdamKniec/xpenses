bbf4d8b7e575beed81c9975cae05fbd299173e03
Adam Knieć <adam.kniec@netguru.com>
2022-02-06 13:53:01 +0100

    adding the form state gathering logic

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,58 @@
 import { Button, Form } from "react-bootstrap";
 
-const ExpensesForm = () => {
+// TODO -> Get Rid of any
+const ExpensesForm = (props: any) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    props.setFormValues((prevState: {}) => {
+      if (e.target.id === "formBasicCheckbox") {
+        return {
+          ...prevState,
+          formBasicCheckbox: e.target.checked,
+        };
+      }
+      return { ...prevState, [e.target.id]: e.target.value };
+    });
+  };
+
   return (
     <div>
       <Form>
         <Form.Group className="mb-3" controlId="price">
           <Form.Label>Ile</Form.Label>
-          <Form.Control type="number" placeholder="Wpisz kwotę" />
+          <Form.Control
+            type="number"
+            placeholder="Wpisz kwotę"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="50/50" onChange={handleChange} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="herShare">
+          <Form.Label>K</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Wpisz kwotę"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="hisShare">
+          <Form.Label>A</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Wpisz kwotę"
+            onChange={handleChange}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="product">
           <Form.Label>Za co</Form.Label>
-          <Form.Control type="product" placeholder="Nazwa produktu / usługi" />
+          <Form.Control
+            type="product"
+            placeholder="Nazwa produktu / usługi"
+            onChange={handleChange}
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
