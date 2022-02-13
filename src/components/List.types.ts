7a03ac589bbfea39b4fd971cd2d901a7ebadc207
Adam Knieć <adam.kniec@netguru.com>
2022-02-13 21:18:28 +0100

    adding the two radio buttons and adding them to a data model

--- src/components/List.types.ts
+++ src/components/List.types.ts
@@ -1,6 +1,11 @@
 import React from "react";
 
 export interface ListProps {
-  data: { productOrService: string; price: number; id: number }[];
+  data: {
+    productOrService: string;
+    price: number;
+    id: number;
+    whoPaid: string;
+  }[];
   removeListItem: (e: React.MouseEvent, id: number) => void; // TODO REMOVE ANY
 }
